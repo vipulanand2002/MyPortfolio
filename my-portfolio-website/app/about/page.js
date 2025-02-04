42be6499f33ca48db0e39ce240f5b9db0d74a5b7
Vipul2002 <karnvipul2002@gmail.com>
2025-02-04 21:10:20 +0530

    Added About page and Css section

---
+++ my-portfolio-website/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+      <section>
+        <h1>About Me</h1>
+        <p>Here's some information about me and my background.</p>
+      </section>
+    );
+  }
